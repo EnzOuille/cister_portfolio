a9265b0ce2bcc0b671d0a7597009768f265f8374
EnzOuille <enzo_cisternino@hotmail.fr>
2025-09-25 23:49:15 +0200

    25/09 Work

--- README.md
+++ README.md
@@ -17,4 +17,18 @@ py -m uvicorn main:app --reload --host 127.0.0.1 --port 8000
 - Avoir la note du film (TheMovieDb)
 - Avoir une note croisée (TheMovieDb + IMDB + LetterboxD)
 
+# Idées 
+
+Actuellement, la recherche et les détails du film sont faits via omdb, la limitation est pour les titres français, le plus intéressant serait de récupérer les résultats globaux avec themoviedb
+pour récup l'id du film souhaité et ensuite faire la recherche détaillée auprès de omdb pour obtenir les détails et les votes, c'est la façon la plus simple et la plus globale possible.
+
+On pourrait également modifier le contenu pour que quand l'on ai pas d'images le contenu prenne toute la largeur. Ou alors on n'affiche pas les contenu sans images.
+
+Ce serait bien d'ajouter un lien vers la fiche du film via l'id récup par themoviedb.
+
+Les résultats doivent être supprimés après avoir sélectionné un film, sinon les anciens résultats sont encore présents même après plusieurs recherches.
+
+Il faudrait harmoniser la page et la rendre plus belle, pour cela il faudrait arrondir l'ensemble, passer dans un fond coloré mais sobre, avec ptet des formes sur le côté de la page.
+
+L'ajout d'un lien pour ajouter le film à sa playlist letterboxd serait aussi une bonne idée.
 *By Enzo Cisternino*
--- WatchIt/backend/main.py
+++ WatchIt/backend/main.py
@@ -17,6 +17,8 @@ app.add_middleware(
 TMDB_READ_KEY = os.getenv("TMDB_READ_KEY", "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MGFhZDdmMDczNzdhN2ZiNWMxOGU4MmY2NDBhYmI0YiIsIm5iZiI6MTc1NjkwNDc2Ni44MDE5OTk4LCJzdWIiOiI2OGI4M2QzZTVlMGQ0N2IwNzU4Y2Q3ZjYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.MC-nQ0bU-z-Rm3vfhEHEnLmQIyGQWWqqXX9masn82oI")
 TMDB_IMAGE_BASE = "https://image.tmdb.org/t/p/w400"  # taille de l'image
 
+OMDB_API_KEY= "f39685d4"
+OMDB_API_URL = "http://www.omdbapi.com/"
 @app.get("/search_tmdb")
 def search_tmdb(movie_name: str = Query(..., description="Titre du film")):
     url = "https://api.themoviedb.org/3/search/movie"
@@ -51,3 +53,53 @@ def search_tmdb(movie_name: str = Query(..., description="Titre du film")):
         })
 
     return results
+
+@app.get("/search_omdb_tomselect")
+def search_omdb_tomselect(movie_name: str = Query(..., description="Titre du film")):
+    params = {
+        "apikey": OMDB_API_KEY,
+        "s": movie_name,
+        "type": "movie",
+        "r": "json"
+    }
+    
+    response = requests.get(OMDB_API_URL, params=params)
+    data = response.json()
+    
+    if "Search" not in data or not data["Search"]:
+        return []
+    
+    results = []
+    for movie in data["Search"]:
+        print(movie)
+        results.append({
+            "value": movie["imdbID"],
+            "text": movie["Title"],
+            "poster": movie["Poster"]
+        })
+        
+    return results
+
+@app.get("/search_omdb_id")
+def search_omdb_id(movie_id: str = Query(..., description="Id du film")):
+    params = {
+        "apikey": OMDB_API_KEY,
+        "i": movie_id,
+        "type": "movie",
+        "r": "json"
+    }
+    
+    response = requests.get(OMDB_API_URL, params=params)
+    data = response.json()
+    
+    result = {
+        "title": data.get("Title", ""),
+        "year": data.get("Year", ""),
+        "runtime": data.get("RunTime",""),
+        "poster": data.get("Poster", ""),
+        "ratings": data.get("Ratings", ""),
+        "plot": data.get("Plot", "")
+    }
+    
+    return result
+    
